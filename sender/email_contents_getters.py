74e939f267d835480193430b2fd495e25bdd90cf
Sihan Yu <sihanyu@user1698.eduroam.christs.private.cam.ac.uk>
2024-10-24 20:07:48 +0100

    Added Cambridge Law Society custom email creation function

--- sender/email_contents_getters.py
+++ sender/email_contents_getters.py
@@ -57,7 +57,7 @@ def families(data: list[tuple[Any, ...]], _, body: str) -> list[EmailContent]:
         """
         Adds to the list email_contents
         :param lst: List of members to add, either the parents or the children
-        :param body: Almost personalised email. Only missing the name part in 'Dear {first_name}'
+        :param body: Almost personalised email. Only missing the name part in 'Dear {receiver}'
         :return: None. Modifies 'email_contents' in-place
         """
         for member in lst:
@@ -97,5 +97,86 @@ def families(data: list[tuple[Any, ...]], _, body: str) -> list[EmailContent]:
 
         add_to_email_contents(families[family_id]['parent'], curr_body)
         add_to_email_contents(families[family_id]['child'], curr_body)
+
+    return email_contents
+
+
+def culs_mentorship_scheme(data: list[tuple[Any, ...]], _, body: str) -> list[EmailContent]:
+    """
+    EmailContent list builder for CULS mentorship scheme emails
+    :param data: Data from SQL table
+    :param body: Email template on which the personalised email will be built on
+    :return: List of the EmailContent objects
+    """
+
+    def generate_table(lst: list[tuple[Any, ...]], tag_start: str = '', tag_end: str = '') -> str:
+        """
+        Generates the table for the email, reused to create both mentor's and mentees' part
+        :param lst: List of the members to build the table on
+        :param tag_start: html tag, namely <b> or an empty string. Used to bold mentor's table
+        :param tag_end: html tag, namely </b> or an empty string. Used to bold mentees' table
+        :return:
+        """
+        table = []
+        for member in lst:
+            first_name, last_name, email = member
+            table.append(f'''<tr>
+                <td>{tag_start}{first_name} {last_name}{tag_end}</td>
+                <td>{tag_start}{email}{tag_end}</td>
+            </tr>''')
+
+        return ''.join(table)
+
+    def add_to_email_contents(lst: list[str], body: str, role: str) -> None:
+        """
+        Adds to the list email_contents
+        :param lst: List of members to add, either the mentor or the mentees
+        :param body: Almost personalised email. Only missing the name part in 'Dear {receiver}', and the opposite role
+        :param role: The role of the members, either 'mentor' or 'mentee'
+        :return: None. Modifies 'email_contents' in-place
+        """
+        for member in lst:
+            first_name = member[0]
+            email = member[2]
+            opposite_role = 'mentor' if role == 'mentee' else 'mentees'
+            is_are = 'is' if role == 'mentee' else 'are'
+            email_contents.append(EmailContent(
+                email=email,
+                body=body.format(
+                    receiver=first_name,
+                    opposite_role=opposite_role,
+                    is_are=is_are
+                )
+            ))
+
+    groups = {}
+
+    for row in data:
+        group_id, first_name, last_name, email, role = row
+
+        if group_id not in groups:
+            groups[group_id] = {'mentor': [], 'mentee': []}
+
+        groups[group_id][role].append((
+            first_name,
+            last_name,
+            email
+        ))
+
+    email_contents = []
+    for group_id in groups:
+        mentor_table = generate_table(groups[group_id]['mentor'], '<b>', '</b>')
+        mentee_table = generate_table(groups[group_id]['mentee'])
+
+        curr_body = body.format(
+            receiver='{receiver}',
+            opposite_role='{opposite_role}',
+            is_are='{is_are}',
+            mentor_table=mentor_table,
+            mentee_table=mentee_table
+        )
+
+        add_to_email_contents(groups[group_id]['mentor'], curr_body, 'mentor')
+        add_to_email_contents(groups[group_id]['mentee'], curr_body, 'mentee')
 
     return email_contents
