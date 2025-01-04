1374c3482b999be1fc8fc25b28b32dd2ed6b0c86
Sihan Yu <sihanyu03@gmail.com>
2025-01-05 00:01:49 +0200

    Fixes and added logging for chatbot

--- chatbot/tools.py
+++ chatbot/tools.py
@@ -14,7 +14,9 @@ def check_if_email_exists(email: str, table: str):
 
     Returns: True if the email exists, False if the email doesn't exist, the error message if there was an error
     """
+    logger.info(f'check_if_email_exists - Called with parameters email={email}, table={table}')
     if not utils.check_sql_variable_validity(table):
+        logger.error(f'check_if_email_exists - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
     try:
         sql_cur.execute(
@@ -22,10 +24,14 @@ def check_if_email_exists(email: str, table: str):
             (email,)
         )
         if sql_cur.fetchall()[0][0]:
-            return f'The email {email} is in the table {table}'
+            return_val = f'The email {email} is in the table {table}'
         else:
-            return f'The email {email} is not in the table {table}'
+            return_val = f'The email {email} is not in the table {table}'
+        logger.info(f'check_if_email_exists - Returned {return_val}')
+        return return_val
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'check_if_email_exists - Error: {str(e)}')
         return str(e)
 
 
@@ -36,7 +42,9 @@ def retrieve_non_cam_and_typo_emails(table: str):
     i.e. have a domain of @cam.ac.uk, and retrieve those emails. Also count how many emails are typos
     of the Cambridge University domain, i.e. are close to @cam.ac.uk
     """
+    logger.info(f'retrieve_non_cam_and_typo_emails - Called with parameters table={table}')
     if not utils.check_sql_variable_validity(table):
+        logger.info(f'retrieve_non_cam_and_typo_emails - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
     try:
         sql_cur.execute(
@@ -51,8 +59,11 @@ def retrieve_non_cam_and_typo_emails(table: str):
                 typos.append(elem[0])
             else:
                 non_cam.append(elem[0])
+        logger.info(f'retrieve_non_cam_and_typo_emails - Returned {len(non_cam)} non-Cambridge emails and {typos} typo emails')
         return f'The number of non-Cambridge domain emails are {len(non_cam)}, Non-Cambridge domain emails are {non_cam}. The number of Cambridge domain emails with typos is {len(typos)}, Cambridge domain emails with typos are {typos}'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'retrieve_non_cam_and_typo_emails - Error: {str(e)}')
         return str(e)
 
 
@@ -71,7 +82,9 @@ def correct_typo_emails(emails: list[str], table: str) -> str:
         emails: List of emails with typos to be corrected
         table: The name of the SQL table to update
     """
+    logger.info(f'correct_typo_emails - Called with parameters emails={emails}, table={table}')
     if not utils.check_sql_variable_validity(table):
+        logger.info(f'correct_typo_emails - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
 
     successful_emails = []
@@ -89,8 +102,11 @@ def correct_typo_emails(emails: list[str], table: str) -> str:
             except psycopg2.Error as psycopg2_e:
                 failed_emails.append((email, str(psycopg2_e)))
         sql_conn.commit()
+        logger.info(f'correct_typo_emails - Returned successful emails={successful_emails}, failed email = {failed_emails}')
         return f'Emails that were successfully fixed: {successful_emails}, emails that failed to be fixed: {failed_emails}'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'correct_typo_emails - Error: {str(e)}')
         return str(e)
 
 
@@ -105,9 +121,12 @@ def create_table(table: str, columns: list[str]):
           table: Name of the table to be created
           columns: List of the column names
     """
+    logger.info(f'create_table - Called with parameters table={table}, columns={columns}')
     if not utils.check_sql_variable_validity(table):
+        logger.error(f'create_table - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
     if not all(utils.check_sql_variable_validity(col) for col in columns):
+        logger.error(f'create_table - Error: Column name(s) invalid')
         return 'Invalid column names, all characters must be alphanumeric or the underscore _ character'
 
     query = [f'CREATE TABLE {table} (id SERIAL PRIMARY KEY,'] + [
@@ -120,15 +139,20 @@ def create_table(table: str, columns: list[str]):
     try:
         sql_cur.execute(query)
         sql_conn.commit()
+        logger.info(f'create_table - Table {table} successfully created')
         return f'Table {table} creation successful'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'create_table - Error: {str(e)}')
         return str(e)
 
 
 @tool
 def delete_table(table: str):
     """Given the name of the SQL database table, delete the whole table."""
+    logger.info(f'delete_table - Called with parameter table={table}')
     if not utils.check_sql_variable_validity(table):
+        logger.info('delete_table - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
 
     try:
@@ -136,8 +160,11 @@ def delete_table(table: str):
             f'DROP TABLE {table}'
         )
         sql_conn.commit()
+        logger.info(f'delete_table - Table {table} successfully deleted')
         return f'Table {table} deletion successful'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'delete_table - Error: {str(e)}')
         return str(e)
 
 
@@ -153,9 +180,12 @@ def insert_into_sql_table(table: str, cols):
          cols: A dictionary containing column-value pairs, where the key is the column, and the value is the value for
          that column to be inserted into the SQL table
     """
+    logger.info(f'insert_into_sql_table - Called with parameters table={table}, cols={cols}')
     if not cols:
+        logger.error('insert_into_sql_table - Error: No column value pairs were provided')
         return 'Error, no column value pairs provided.'
     if not utils.check_sql_variable_validity(table):
+        logger.error('insert_into_sql_table - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
 
     columns = ','.join(cols.keys())
@@ -166,22 +196,30 @@ def insert_into_sql_table(table: str, cols):
     try:
         sql_cur.execute(query, values)
         sql_conn.commit()
+        logger.info('insert_into_sql_table - Inserting row was successful')
         return f'Inserting the row was successful'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'insert_into_sql_table - Error: {str(e)}')
         return str(e)
 
 
 @tool
 def retrieve_table_length(table: str):
     """Given the name of a SQL table, retrieve the length of the table"""
+    logger.info(f'retrieve_table_length - Called with parameter table={table}')
     if not utils.check_sql_variable_validity(table):
+        logger.error(f'retrieve_table_length - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
 
     try:
         sql_cur.execute(f'SELECT COUNT(*) FROM {table}')
         result = sql_cur.fetchone()[0]
+        logger.info(f'retrieve_table_length - Returned {result} as the length of the table {table}')
         return f'The query gave the length of {table} as {result}'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'retrieve_table_length - Error: {str(e)}')
         return str(e)
 
 
@@ -198,9 +236,12 @@ def modify_element(table: str, identifier_column_name: str, identifier_value: st
         column_to_be_modified: The column that will be modified
         new_value: The new value that will be put into the column_to_be_modified column
     """
+    logger.info(f'modify_element - Called with parameters table={table}, identifier_column_name={identifier_column_name}, identifier_value={identifier_value}, column_to_be_modified={column_to_be_modified}, new_value={new_value}')
     if not utils.check_sql_variable_validity(table):
+        logger.error('modify_element - Error: Table name invalid')
         return 'Invalid table name, all characters in the table have to be alphanumeric or the underscore _ character'
     if not utils.check_sql_variable_validity(identifier_column_name) or not utils.check_sql_variable_validity(column_to_be_modified):
+        logger.error('modify_element - Error: Column name(s) invalid')
         return 'Invalid column names, all characters in the table have to be alphanumeric or underscode _ character'
 
     try:
@@ -210,8 +251,11 @@ def modify_element(table: str, identifier_column_name: str, identifier_value: st
         )
         prev_value = sql_cur.fetchone()[0]
         sql_conn.commit()
+        logger.info(f'modify_element - Row successfully updated, value {prev_value} updated to {new_value}')
         return f'Row updated, value {prev_value} successfully updated to {new_value}'
     except Exception as e:
+        sql_conn.rollback()
+        logger.error(f'modify_element - Error: {str(e)}')
         return str(e)
 
 
@@ -232,3 +276,5 @@ sql_conn = psycopg2.connect(
     password='password'
 )
 sql_cur = sql_conn.cursor()
+
+logger = utils.get_logger()
--- chatbot/utils.py
+++ chatbot/utils.py
@@ -1,3 +1,7 @@
+import os
+import logging
+
+
 def hamming_distance(a: str, b: str):
     if len(a) == len(b):
         return sum(c1 != c2 for c1, c2 in zip(a, b))
@@ -6,4 +10,19 @@ def hamming_distance(a: str, b: str):
 
 def check_sql_variable_validity(variable: str):
     return all(c.isalnum() or c == '_' for c in variable)
+
+
+def get_logger():
+    """
+    :return: Logger object that is to be used in the whole program
+    """
+    logger = logging.getLogger('logger')
+    logging.basicConfig(
+        filename=os.path.join(os.path.dirname(__file__), 'chatbot.log'),
+        filemode='a',
+        format=f'%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.DEBUG
+    )
+
+    return logger
 
